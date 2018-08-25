df33749e9ddefbc10ce91a865173be91e99903cd
Quan Nguyen <quannguyen1456@gmail.com>
2018-08-25 21:28:31 +0700

    master: show all email and show email detail

--- index.php
+++ index.php
@@ -10,8 +10,36 @@
     <title>Document</title>
     <script type='text/javascript' src='knockout-3.4.2.js'></script>
     <style>
+        .folders {
+            background-color: #bbb;
+            list-style-type: none;
+            padding: 0;
+            margin: 0;
+            border-radius: 7px;
+            background-image: -webkit-gradient(linear, left top, left bottom, color-stop(0, #d6d6d6), color-stop(0.4, #c0c0c0), color-stop(1, #a4a4a4));
+            margin: 10px 0 16px 0;
+            font-size: 0px;
+        }
+
+        .folders li {
+            font-size: 16px;
+            font-weight: bold;
+            display: inline-block;
+            padding: 0.5em 1.5em;
+            cursor: pointer;
+            color: #444;
+            text-shadow: #f7f7f7 0 1px 1px;
+            border-left: 1px solid #ddd;
+            border-right: 1px solid #888;
+        }
+
         .folders .selected {
-            color: red;
+            background-color: #444 !important;
+            color: white;
+            text-shadow: none;
+            border-right-color: #aaa;
+            border-left: none;
+            box-shadow: inset 1px 2px 6px #070707;
         }
     </style>
     <script
@@ -20,29 +48,62 @@
             crossorigin="anonymous"></script>
 </head>
 <body>
+<!-- Todo: Create UI -->
 <ul class="folders" data-bind="foreach: folders">
-    <li data-bind="text: $data, css: { selected : $data == $root.chosenFolderId() }, click: $root.goToFolder"></li>
+    <li data-bind="text: $data, css: { selected: $data == $root.chosenFolderId() }, click: $root.goToFolder"></li>
 </ul>
+<!-- Mails grid -->
 <table class="mails" data-bind="with: chosenFolderData">
-    <thead><tr><th>From</th><th>To</th><th>Subject</th><th>Date</th></tr></thead>
+    <thead>
+    <tr>
+        <th>From</th>
+        <th>To</th>
+        <th>Subject</th>
+        <th>Date</th>
+    </tr>
+    </thead>
     <tbody data-bind="foreach: mails">
-        <tr>
-            <td data-bind="text: from"></td>
-            <td data-bind="text: to"></td>
-            <td data-bind="text: subject"></td>
-            <td data-bind="text: date"></td>
-        </tr>
+    <tr data-bind="click: $root.goToMail">
+        <td data-bind="text: from"></td>
+        <td data-bind="text: to"></td>
+        <td data-bind="text: subject"></td>
+        <td data-bind="text: date"></td>
+    </tr>
     </tbody>
 </table>
+<!-- Chosen mail -->
+<div class="viewMail" data-bind="with: chosenMailData">
+    <div class="mailInfo">
+        <h1 data-bind="text: subject"></h1>
+        <p><label>From</label>: <span data-bind="text: from"></span></p>
+        <p><label>To</label>: <span data-bind="text: to"></span></p>
+        <p><label>Date</label>: <span data-bind="text: date"></span></p>
+    </div>
+    <p class="message" data-bind="html: messageContent" />
+</div>
 <script type="text/javascript">
     function WebmailViewModel() {
+        // Data
         var self = this;
         self.folders = ['Inbox', 'Archive', 'Sent', 'Spam'];
         self.chosenFolderId = ko.observable();
-        self.chosenFolderData = ko.observable();
-        self.goToFolder = function(folder) {
+        self.chosenMailData = ko.observable();
+        self.chosenFolderData = ko.observable({mails: [{from: 10, to: 5, subject: 5, date: 55}]});
+        self.goToFolder = function (folder) {
             self.chosenFolderId(folder);
-            $.get('/mail', { folder: folder }, self.chosenFolderData);
+            self.chosenMailData(null);
+            $.get('/knockoutjs/mail.php', {folder: folder}, function (returnValue) {
+                var obj = JSON.parse(returnValue);
+                self.chosenFolderData(obj);
+            });
+        };
+        self.goToMail = function(mail) {
+            self.chosenFolderId(mail.folder);
+            self.chosenFolderData(null);
+            $.get("/knockoutjs/mail.php", { mailId: mail.id }, function (returnValue) {
+                var obj = JSON.parse(returnValue);
+                self.chosenMailData(obj);
+            });
         };
         self.goToFolder('Inbox');
     };
---
+++ mail.php
@@ -0,0 +1,24 @@
+<?php
+
+$type = $_REQUEST['folder'];
+
+$mailId = $_REQUEST['mailId'];
+
+if ($type) {
+    switch ($type) {
+        case 'Inbox' :
+            echo '{ "mails": [{"id": 1, "from": 15, "to": 33, "subject": 5, "date": 55}]}';
+            break;
+        case 'Archive':
+            echo '{ "mails": [{"id": 2, "from": 20, "to": 20, "subject": 5, "date": 55}]}';
+            break;
+        default:
+            echo '{ "mails": [{"id": 3, "from": 10, "to": 10, "subject": 5, "date": 55}]}';
+            break;
+    }
+}
+
+if ($mailId) {
+    echo '{"id":1,"from":"Abbot \u003coliver@smoke-stage.xyz\u003e","to":"steve@example.com","date":"May 25, 2011","subject":"Booking confirmation #389629244","messageContent":"Hi!\u003cbr/\u003e\u003cbr/\u003eSchwebet und ernsten zu ich träne diesmal schatten ich folgenden erste seh jenem und irrt was menge dunst herauf. Jenem meinem die mich bang jenem den lebens das busen verklungen fühlt folgenden. Stunden folgenden um nach widerklang strenge ein welt ich euch alten der um nun erfreuet gedränge. Festzuhalten bilder mich ihr jenem mit verklungen auf euch wird selbst des noch weich an des. Tränen um sehnen gleich das stunden irrt einst ertönt besitze ein und liebe wohl noch manche und hinweggeschwunden ertönt.\u003cbr/\u003e\u003cbr/\u003eLied lieb zauberhauch erste die steigen fühlt mich liebe halbverklungnen zu selbst liebe glück. Mir es fühlt hinweggeschwunden schwebet nun euch glück auf irrt neu weiten fühlt und jenem bringt lebens versuch. Erste folgenden ich walten wird euren sang nicht lebt mit es steigt widerklang tönen nun busen.\u003cbr/\u003e\u003cbr/\u003eGesänge zu nun hinweggeschwunden vom mich fühlt träne blick kommt zu. Um wohl es freundliche denen geneigt wird. Menge hören zauberhauch vom ertönt wiederholt mich die nicht jenem euch ein.\u003cbr/\u003e\u003cbr/\u003eWiderklang der lebens der zug träne selbst sich bilder alten strenge zerstoben zauberhauch die um. Ertönt versuch erfreuet und. Ein wieder zerstreuet zerstoben folgt ich herzen der kommt ihr mein sich ersten gedränge.\u003cbr/\u003e\u003cbr/\u003eBest regards - Jonas","folder":"Inbox"}';
+}
+
