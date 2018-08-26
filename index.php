4b2f202eab82f42a8773f54c01d759304a8d5e1f
Quan Nguyen <quannguyen1456@gmail.com>
2018-08-26 10:41:40 +0700

    master: creating custom bindings

--- index.php
+++ index.php
@@ -10,127 +10,111 @@
     <title>Document</title>
     <script type='text/javascript' src='knockout-3.4.2.js'></script>
     <style>
-        .folders {
-            background-color: #bbb;
-            list-style-type: none;
-            padding: 0;
-            margin: 0;
-            border-radius: 7px;
-            background-image: -webkit-gradient(linear, left top, left bottom, color-stop(0, #d6d6d6), color-stop(0.4, #c0c0c0), color-stop(1, #a4a4a4));
-            margin: 10px 0 16px 0;
-            font-size: 0px;
-        }
-
-        .folders li {
-            font-size: 16px;
-            font-weight: bold;
+        .starRating span {
+            width: 24px;
+            height: 24px;
+            background-image: url(stars.png);
             display: inline-block;
-            padding: 0.5em 1.5em;
             cursor: pointer;
-            color: #444;
-            text-shadow: #f7f7f7 0 1px 1px;
-            border-left: 1px solid #ddd;
-            border-right: 1px solid #888;
+            background-position: -24px 0;
         }
-
-        .folders .selected {
-            background-color: #444 !important;
-            color: white;
-            text-shadow: none;
-            border-right-color: #aaa;
-            border-left: none;
-            box-shadow: inset 1px 2px 6px #070707;
+        .starRating span.chosen {
+            background-position: 0 0;
+        }
+        .starRating span.hoverChosen {
+            background-position: 0 0;
         }
     </style>
+    <link rel="stylesheet" href="jquery-ui.css">
     <script
             src="https://code.jquery.com/jquery-2.2.4.min.js"
             integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="
             crossorigin="anonymous"></script>
     <script src="sammy.min.js"></script>
+    <script src="jquery-ui.min.js"></script>
 </head>
 <body>
-<!-- Todo: Create UI -->
-<ul class="folders" data-bind="foreach: folders">
-    <li data-bind="text: $data, css: { selected: $data == $root.chosenFolderId() }, click: $root.goToFolder"></li>
-</ul>
-<!-- Mails grid -->
-<table class="mails" data-bind="with: chosenFolderData">
-    <thead>
+<h3 data-bind="text: question"></h3>
+<p>Please distribute <b data-bind="text: pointsBudget"></b> points between the following options.</p>
+
+<table>
+    <thead><tr><th>Option</th><th>Importance</th></tr></thead>
+    <tbody data-bind="foreach: answers">
     <tr>
-        <th>From</th>
-        <th>To</th>
-        <th>Subject</th>
-        <th>Date</th>
-    </tr>
-    </thead>
-    <tbody data-bind="foreach: mails">
-    <tr data-bind="click: $root.goToMail">
-        <td data-bind="text: from"></td>
-        <td data-bind="text: to"></td>
-        <td data-bind="text: subject"></td>
-        <td data-bind="text: date"></td>
+        <td data-bind="text: answerText"></td>
+        <td data-bind="starRating: points"></td>
     </tr>
     </tbody>
 </table>
-<!-- Chosen mail -->
-<div class="viewMail" data-bind="with: chosenMailData">
-    <div class="mailInfo">
-        <h1 data-bind="text: subject"></h1>
-        <p><label>From</label>: <span data-bind="text: from"></span></p>
-        <p><label>To</label>: <span data-bind="text: to"></span></p>
-        <p><label>Date</label>: <span data-bind="text: date"></span></p>
-    </div>
-    <p class="message" data-bind="html: messageContent" />
-</div>
+
+<h3 data-bind="fadeVisible: pointsUsed() > pointsBudget">You've used too many points! Please remove some.</h3>
+<p>You've got <b data-bind="text: pointsBudget - pointsUsed()"></b> points left to use.</p>
+<button data-bind="jqButton: {enable: pointsUsed() <= pointsBudget}, click: save">Finished</button>
 <script type="text/javascript">
-    function WebmailViewModel() {
-        // Data
-        var self = this;
-        self.folders = ['Inbox', 'Archive', 'Sent', 'Spam'];
-        self.chosenFolderId = ko.observable();
-        self.chosenMailData = ko.observable();
-        self.chosenFolderData = ko.observable({mails: [{id : 1, folder: 'Inbox', from: 10, to: 5, subject: 5, date: 55}]});
-        self.goToFolder = function (folder) {
-            location.hash = folder;
-            self.chosenFolderId(folder);
-            self.chosenMailData(null);
-            $.get('/knockoutjs/mail.php', {folder: folder}, function (returnValue) {
-                var obj = JSON.parse(returnValue);
-                self.chosenFolderData(obj);
-            });
-        };
-        self.goToMail = function(mail) {
-            location.hash = mail.folder + '/' + mail.id;
-            self.chosenFolderId(mail.folder);
-            self.chosenFolderData(null);
-            $.get("/knockoutjs/mail.php", { mailId: mail.id }, function (returnValue) {
-                var obj = JSON.parse(returnValue);
-                self.chosenMailData(obj);
-            });
-        };
-        Sammy(function() {
-            this.get('#:folder', function() {
-                self.chosenFolderId(this.params.folder);
-                self.chosenMailData(null);
-                $.get("mail.php", { folder: this.params.folder }, function (returnValue) {
-                    var obj = JSON.parse(returnValue);
-                    self.chosenFolderData(obj);
-                });
-            });
+    function Answer(text) { this.answerText = text; this.points = ko.observable(1); }
 
-            this.get('#:folder/:mailId', function() {
-                self.chosenFolderId(this.params.folder);
-                self.chosenFolderData(null);
-                $.get("mail.php", { mailId: this.params.mailId }, function (returnValue) {
-                    var obj = JSON.parse(returnValue);
-                    self.chosenMailData(obj);
+    function SurveyViewModel(question, pointsBudget, answers) {
+        ko.bindingHandlers.starRating = {
+            init: function(element, valueAccessor){
+                $(element).addClass('starRating');
+                var observable = valueAccessor();
+                for (var i = 0; i < 5; i++)
+                    $("<span>").appendTo(element);
+                $("span", element).each(function(index){
+                    $(this).hover(
+                        function() { $(this).prevAll().add(this).addClass("hoverChosen")},
+                        function() { $(this).prevAll().add(this).removeClass("hoverChosen") }
+                    ).click(function(){
+                        var observable = valueAccessor();
+                        observable(index + 1);
+                    });
                 });
-            });
-            this.get('', function() { this.app.runRoute('get', 'knockoutjs/#Inbox') });
-        }).run();
-    };
+            },
+            update: function(element, valueAccessor) {
+                var observable = valueAccessor();
+                $("span", element).each(function(index) {
+                    $(this).toggleClass("chosen", index < observable());
+                });
+            }
+        },
+        ko.bindingHandlers.jqButton = {
+            init: function(element) {
+                $(element).button();
+            },
+            update: function(element, valueAccessor) {
+                var currentValue = valueAccessor();
+                $(element).button("option", "disabled", currentValue.enable === false);
+            }
+        },
+        ko.bindingHandlers.fadeVisible = {
+            init: function(element, valueAccessor) {
+                var shouldDisplay = valueAccessor();
+                $(element).toggle(shouldDisplay);
+            },
+            update: function(element, valueAccessor) {
+                var shouldDisplay = valueAccessor();
+                shouldDisplay ? $(element).fadeIn() : $(element).fadeOut();
+            }
+        }
+        this.question = question;
+        this.pointsBudget = pointsBudget;
+        this.answers = $.map(answers, function(text) { return new Answer(text) });
+        this.save = function() { alert('To do') };
 
-    ko.applyBindings(new WebmailViewModel());
+        this.pointsUsed = ko.computed(function() {
+            var total = 0;
+            for (var i = 0; i < this.answers.length; i++)
+                total += this.answers[i].points();
+            return total;
+        }, this);
+    }
+
+    ko.applyBindings(new SurveyViewModel("Which factors affect your technology choices?", 10, [
+        "Functionality, compatibility, pricing - all that boring stuff",
+        "How often it is mentioned on Hacker News",
+        "Number of gradients/dropshadows on project homepage",
+        "Totally believable testimonials on project homepage"
+    ]));
 </script>
 </body>
 </html>
